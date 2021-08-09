5aec833b9b4e61c8c33cbd647d6afa089ed9870a
Kevin Kappelmann <accounts@kappelmann.me>
2021-08-09 23:37:45 +0200

    feat(log,utils) minor rename and refactor

--- Hint_Unification.thy
+++ Hint_Unification.thy
@@ -4,14 +4,13 @@ imports
   Spec_Check2.Spec_Check
 begin
 
-ML_file\<open>Log.ML\<close>
-ML_file\<open>Utils.ML\<close>
+ML_file\<open>logger.ML\<close>
+ML_file\<open>utils.ML\<close>
 
 named_theorems hints
 
 ML_file\<open>Hint_Unification.ML\<close>
 ML_file\<open>FO_Hint_Unification.ML\<close>
 ML_file "HO_Pat_Hint_Unification.ML"
-
 
 end
