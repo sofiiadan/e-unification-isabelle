731da8fa4a698a8110ac5f7ea81cbcf81bf637dc
Kevin Kappelmann <accounts@kappelmann.me>
2023-02-03 19:26:35 +0100

    feat(*) fix bugs (eta-norm, Seq.is_empty, PATTERN exception)

--- E_Unification.thy
+++ E_Unification.thy
@@ -10,11 +10,12 @@ text \<open>Unification modulo equations.\<close>
 ML_file\<open>unification_base.ML\<close>
 ML_file\<open>util.ML\<close>
 
-named_theorems hints
 ML_file\<open>unification_hints.ML\<close>
 
 ML_file\<open>higher_order_pattern_unification.ML\<close>
 ML_file\<open>first_order_unification.ML\<close>
+
+ML_file\<open>unif_resolve.ML\<close>
 
 
 end
